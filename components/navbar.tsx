ef7afd13293216b0b7856a109618b7e2aa86124e
Samuel-Omobuwa <samuelomobuwa1@gmail.com>
2026-02-25 23:52:05 +0100

    added features

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,8 +1,23 @@
+import { Briefcase } from "lucide-react";
+import Link from "next/link";
+import { Button } from "./ui/button";
+
 export default function Navbar(){
     return (
         <nav>
             <div>
-                Link
+                <Link href="/" className="flex item-center gap-2 text-xl font-semibold text-primary">
+                <Briefcase  />
+                JOb Tracker</Link>
+
+                
+                <div>
+                    <Link href='/sign-in'>
+                    <Button variant="ghost" className="text-gray-700 hover:text-black">Log in</Button></Link>
+
+                    <Link href='/sign-up'>
+                    <Button className="bg-primary hover:bg-primary/90">Sign up</Button></Link>
+                </div>
             </div>
         </nav>
     )
